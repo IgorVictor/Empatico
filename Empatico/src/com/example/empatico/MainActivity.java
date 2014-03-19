fa01dfe105aae806e14cfdce5c7e3d699b113306
Eldandil <rafaeleldandil@gmail.com>
2014-03-19 06:34:16 -0300

    ImageFields
    Troquei o texto por imagens e fiz um novo adapter. Se eu conseguir
    trocar de Image pra Button de vez, posso passar para gerar a tela
    automaticamente

--- Empatico/src/com/example/empatico/MainActivity.java
+++ Empatico/src/com/example/empatico/MainActivity.java
@@ -2,35 +2,78 @@ package com.example.empatico;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
-	GridView gridView;
-    
-    static final String[] numbers = new String[] {
-
-            "Botão1", "Botão2", "Botão3", "Botão4", "Botão5",
-            "Botão", "Botão", "Botão", "Botão", "Substituir texto por botão"
-        };
+    private Integer[] mThumbIds = {R.drawable.ic_launcher,
+    		R.drawable.ic_launcher, R.drawable.ic_launcher,
+    		R.drawable.ic_launcher, R.drawable.ic_launcher,
+    		R.drawable.ic_launcher, R.drawable.ic_launcher,
+    		R.drawable.ic_launcher, R.drawable.ic_launcher,
+    		R.drawable.ic_launcher, R.drawable.ic_launcher};
  
+ 
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    
         super.onCreate(savedInstanceState);
- 
         setContentView(R.layout.activity_main);
  
-        gridView = (GridView) findViewById(R.id.gridview1);  
+        GridView gridview = (GridView) findViewById(R.id.gridview1);
+        gridview.setAdapter(new MyAdapter(this));
+        gridview.setNumColumns(4);
+    }
  
-      // Create adapter to set value for grid view
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, numbers);
+  public class MyAdapter extends BaseAdapter {
  
-        gridView.setAdapter(adapter);
+        private Context mContext;
  
+        public MyAdapter(Context c) {
+            mContext = c;
+        }
+ 
+        @Override
+        public int getCount() {
+            return mThumbIds.length;
+        }
+ 
+        @Override
+        public Object getItem(int arg0) {
+            return mThumbIds[arg0];
+        }
+ 
+        @Override
+        public long getItemId(int arg0) {
+            return arg0;
+        }
+ 
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+ 
+            View grid;
+ 
+            if(convertView==null){
+                grid = new View(mContext);
+                LayoutInflater inflater=getLayoutInflater();
+                grid=inflater.inflate(R.layout.buttonlayout, parent, false);
+            }else{
+                grid = (View)convertView;
+            }
+ 
+            ImageView imageView = (ImageView)grid.findViewById(R.id.image);
+            imageView.setImageResource(mThumbIds[position]);
+ 
+            return grid;
+        }
     }
 
 }
