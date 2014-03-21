c684044290fb06edbadc3662cbdceec5011e74e9
Eldandil <rafaeleldandil@gmail.com>
2014-03-21 13:16:19 -0300

    Refactoring
    Eu dei uma limpada na classe. Refatorei. Quebrei as declarações imensas
    e métodos imensos em métodos menores. Generalizei as definições de
    layout(tamanho de tudo) e generalizei de vez o numero de botões exibidos
    por linha. Agora é só criar o método que tem a lógica de setar os
    tamanhos de acordo com o tamanho da tela e adaptar algumas coisas
    pequenas.

--- Empatico/src/com/example/empatico/Empatico.java
+++ Empatico/src/com/example/empatico/Empatico.java
@@ -1,12 +1,10 @@
 package com.example.empatico;
 
 import android.app.Activity;
-import android.graphics.Color;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.HorizontalScrollView;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -21,61 +19,87 @@ public class Empatico extends Activity implements View.OnClickListener {
 			super.onCreate(savedInstanceState);
 			
 			maxPerLine = 5;
-			int[] buttonImages = {R.drawable.banheiro, R.drawable.beber, R.drawable.brincar, R.drawable.comer,
-					R.drawable.dormir, R.drawable.mal_estar, R.drawable.sair, R.drawable.triste, R.drawable.vestir};
-			LinearLayout layoutInicial = new LinearLayout(this);
-			HorizontalScrollView horizontalLayout = new HorizontalScrollView(this);
-			ScrollView verticalLayout = new ScrollView(this);
-			TableLayout layout = new TableLayout(this);
-			layout.setBackgroundResource(R.drawable.tela_back);
-			horizontalLayout.addView(verticalLayout);
-			verticalLayout.addView(layout);
-			ImageView image = new ImageView(this);
-			image.setImageResource(R.drawable.tela_inicial);
-			layoutInicial.addView(image);
+			int xTam = 200;
+			int yTam = 200;
+			int top = 0;
+			int left = 10;
+			int right = 0;
+			int bottom = 10;
+			int[] buttonImages = prepareImageArray();
+			HorizontalScrollView horizontalLayout = prepareHorizontal();
+			ScrollView verticalLayout = prepareVertical();
+			TableLayout layout = prepareTable();
+			mountLayout(horizontalLayout, verticalLayout,
+					layout);
+			setButtonsOnLayout(xTam, yTam, buttonImages, layout, left, top, right, bottom);
+			super.setContentView(horizontalLayout);
+		}
 
-			layout.setPadding(1,1,1,1);
+		private void setButtonsOnLayout(int xTam, int yTam, int[] buttonImages,
+				TableLayout layout, int left, int top, int right, int bottom) {
 			int i = 0;
 			TableRow tr = new TableRow(this);
+			tr.setPadding(left, top, right, bottom);
+			
+			HelpButton config = createButton(R.drawable.settings,this);
+			tr.addView(config);
 			while (i<buttonImages.length){
 				if(i%maxPerLine == 0){
 					layout.addView(tr);
 					tr = new TableRow(this);
-					HelpButton b = new HelpButton(this);
-					b.setImageResource(buttonImages[i]);
-					b.setOnClickListener(this);
-					tr.addView(b, 200,200);
+					tr.setPadding(0, 10, 0, 10);
+					HelpButton button = createButton(buttonImages[i],this);
+					button.setOnClickListener(this);
+					tr.addView(button, xTam,yTam);
 					i++;
 				}
 				else {
-					HelpButton b = new HelpButton (this);
-					b.setImageResource(buttonImages[i]);
-					b.setOnClickListener(this);
-					tr.addView(b, 200,200);
+					HelpButton button = createButton(buttonImages[i],this);
+					button.setOnClickListener(this);
+					tr.addView(button, xTam,yTam);
 					i++;
 				}
 			}
 			if (!(i%maxPerLine == 0)){
 				layout.addView(tr);
 			}
-			
-			
-/*			for (int f=0; f<2; f++) {
-				TableRow tr = new TableRow(this);
-				for (int c=0; c<5; c++) {
-					HelpButton b = new HelpButton (this);
-					b.setImageResource(buttonImages[i]);
-					b.setOnClickListener(this);
-					tr.addView(b, 200,200);
-					if (i<buttonImages.length-1){
-						i++;
-					}
-				} // for
-				layout.addView(tr);
-			} // for
-*/			super.setContentView(layoutInicial);
-			super.setContentView(horizontalLayout);
-		} // ()
+		}
+		private HelpButton createButton(int imageId, Context context){
+			HelpButton b = new HelpButton(context);
+			b.setImageResource(imageId);
+			return b;
+		}
+
+		private void mountLayout(
+				HorizontalScrollView horizontalLayout,
+				ScrollView verticalLayout, TableLayout layout) {
+			horizontalLayout.addView(verticalLayout);
+			verticalLayout.addView(layout);
+		}
+
+		private TableLayout prepareTable() {
+			TableLayout layout = new TableLayout(this);
+			layout.setBackgroundResource(R.drawable.tela_back);
+			return layout;
+		}
+
+		private ScrollView prepareVertical() {
+			ScrollView verticalLayout = new ScrollView(this);
+			verticalLayout.setFillViewport(true);
+			return verticalLayout;
+		}
+
+		private HorizontalScrollView prepareHorizontal() {
+			HorizontalScrollView horizontalLayout = new HorizontalScrollView(this);
+			horizontalLayout.setFillViewport(true);
+			return horizontalLayout;
+		}
+
+		private int[] prepareImageArray() {
+			int[] buttonImages = {R.drawable.banheiro, R.drawable.beber, R.drawable.brincar, R.drawable.comer,
+					R.drawable.dormir, R.drawable.mal_estar, R.drawable.sair, R.drawable.triste, R.drawable.vestir};
+			return buttonImages;
+		} 
 
 		public void onClick(View view) {
 	
