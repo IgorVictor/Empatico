47e0972c6a6caa782c93626eca69c45cc5fdc93b
walter <walter.arruda.alves@gmail.com>
2014-03-26 14:01:37 -0300

    adicionado o listener do ListView

--- Empatico/src/com/example/empatico/Config.java
+++ Empatico/src/com/example/empatico/Config.java
@@ -10,14 +10,18 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ScrollView;
+import android.widget.Toast;
 
 
 public class Config extends Activity{
 
+	Component acaoEditar = null;
 	private List<Component> components;
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -40,25 +44,33 @@ public class Config extends Activity{
 			}
 		});
 		ListView listaAcoes = (ListView) findViewById(R.id.listaAcoes);
-		final StableArrayAdapter adapter = new StableArrayAdapter(this,R.layout.text, components);
-		listaAcoes.setAdapter(adapter);
-		/*		listaAcoes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-			@Override
-			public void onItemClick(AdapterView<?> parent, final View view,
-					int position, long id) {
-				final String item = (String) parent.getItemAtPosition(position);
-				list.remove(item);
-				adapter.notifyDataSetChanged();
-			}
-
-		});*/
-		///////////////////////////////////
 		Button criarAcao = (Button) findViewById(R.id.criarNovaAcao);
 		Button excluirAcao = (Button) findViewById(R.id.excluirAcao);
 		EditText nomeDependente = (EditText) findViewById(R.id.nomeDependente);
 		EditText novaSenha = (EditText) findViewById(R.id.novaSenha);
+		final EditText novaMsg = (EditText) findViewById(R.id.helpText);
+		final EditText novaImg = (EditText) findViewById(R.id.imagePath);
+		//final StableArrayAdapter adapter = new StableArrayAdapter(this,R.layout.text, components);
+		
+		ArrayAdapter<Component> adapter = new ArrayAdapter<Component>(this, android.R.layout.simple_list_item_checked, components );
+		
+		listaAcoes.setAdapter(adapter);
+		
+		listaAcoes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> adapter, View arg1, int posicao,
+					long arg3) {
+								
+				acaoEditar = (Component) adapter.getItemAtPosition(posicao);
+				novaMsg.setText(acaoEditar.getMsgToSend());
+				novaImg.setText(acaoEditar.getImagePath());
+			}
+			
+		});
+		
 	}
+		
 	private ScrollView prepareVertical() {
 		ScrollView verticalLayout = new ScrollView(this);
 		verticalLayout.setFillViewport(true);
